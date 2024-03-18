b18c1dcf76982d44f20298927c02efe67158ae4b
Saloni Zade <salonizade09@gmail.com>
2024-03-18 11:19:46 +0530

    Add day 2 changes for NextHeirs project

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {GrInherit} from 'react-icons/gr'
+const Header = () => {
+  return (
+    <header className='flex justify-between items-center p-5 bg-white text-gray-500
+    hover:text-gray-700 shadow-lg fixed top-0 left-0 right-0'>
+        <a href="#" className='flex justify-start items-center text-xl text-black space-x-1'>
+            <span>NextHeirs</span>
+            <GrInherit/>
+        </a>
+
+        <div className='flex space-x-2 justify-center'>
+            <button type='button'
+            className='inline-block px-6 py-2.5 bg-green-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-green-700 '>Connect Wallet
+            </button>
+        </div>
+        </header>
+  )
+}
+
+export default Header
