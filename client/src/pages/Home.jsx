90171caf16c85eb05a364db3169bc8ef67af2647
vizzscript <script.vizz@gmail.com>
2024-03-21 11:04:00 +0530

    Add day-4 changes for NextHeirs project

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Header from '../components/Header';
+import Connect from '../components/Connect';
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
 const Home = () => {
   return (
     <div className="h-screen w-full">
+      <Header />
       <div className="flex flex-col items-center justify-center h-full w-full space-x-2 bg-black">
         <div className="p-4 m-4">
           <h1 className="text-4xl sm:text-7xl font-bold bg-gradient-to-r from-green-900 via-green-400 to-green-300 bg-clip-text text-transparent text-center">Welcome to NextHeirs!!</h1>
@@ -11,11 +16,14 @@ const Home = () => {
         <div className="space-x-2 flex
                 justify-center items-center mt-10 flex-col
                 md:flex-row">
-          <Link to="/Registration" className="inline-block h-16 w-56 px-6 py-2.5 bg-green-700  text-white font-medium text-xl leading-tight uppercase rounded-full shadow-md hover:bg-transparent hover:text-green-600 hover:border border-green-600">Register</Link>
+          <Link to="/registration" className="inline-block h-16 w-56 px-6 py-2.5 bg-green-700  text-white font-medium text-xl leading-tight uppercase rounded-full shadow-md hover:bg-transparent hover:text-green-600 hover:border border-green-600">Register</Link>
           <br />
-          <Link to="/Login" className="inline-block h-16 w-56 px-4 py-2.5 border border-green-600  text-green-600 font-medium text-xl leading-tight uppercase rounded-full shadow-md hover:bg-green-700 bg-transparent hover:text-white">Login</Link>
+          <Link to="/login" className="inline-block h-16 w-56 px-4 py-2.5 border border-green-600  text-green-600 font-medium text-xl leading-tight uppercase rounded-full shadow-md hover:bg-green-700 bg-transparent hover:text-white">Login</Link>
         </div>
       </div>
+      <Hero/>
+      <Connect/>
+      <Footer/>
     </div>
   );
 };
