a33533cbfbca4004d14462d55382ed2a11dda1e5
Akshay Dhobale <akshaysd592@gamil.com>
2024-03-22 13:16:38 +0530

    react toast added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {GoogleOAuthProvider} from '@react-oauth/google';
+import {Toaster} from 'react-hot-toast'
 
 import {Provider} from 'react-redux'
 import {configureStore} from '@reduxjs/toolkit'
 import rootReducer from './reducer/index.js'
+import { Toast } from 'react-bootstrap';
 
 
 const store = configureStore({
@@ -18,8 +20,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
   <Provider  store={store}>
       <App />
+      <Toaster/>
   </Provider> 
-
-    <App/>
   </React.StrictMode>,
 )
