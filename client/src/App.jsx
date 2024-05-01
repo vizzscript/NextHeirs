06871f46b4ee2ab0912129bf31b8372fa3336a4e
Saloni0111-cpu <salonizade09@gmail.com>
2024-05-01 15:39:55 +0530

    Some new changes added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,6 @@
 import Home from "./pages/Home";
-import Registration from "./components/Registration"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from "./components/Login";
 import User from "./components/User"
-import Error from './components/Error'
 export default function App() {
   return (
       <div className="min-h-screen relative">
