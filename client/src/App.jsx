ef65f55a4df701bbc53baf11771676a0271e9b2a
Saloni0111-cpu <salonizade09@gmail.com>
2024-05-01 15:52:12 +0530

    made new changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import User from "./components/User"
-import Error from './components/Error'
-import Wills from "./components/Wills";
+
 export default function App() {
   return (
       <div className="min-h-screen relative">
       <Router>
         <Routes>
           <Route path="/" element={<User />} />
-          <Route path="/login/user/wills" elements={<Wills/>} />
-          <Route path="*" element={<Error/>}/>
+          {/* <Route path="/login/user/wills" elements={<Wills/>} /> */}
         </Routes>
       </Router>
     </div>
